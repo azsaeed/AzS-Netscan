c3ce28bef438e37757702606c447677269f06e45
azsaeed <42968996+azsaeed@users.noreply.github.com>
2018-09-04 15:04:52 +0200

    Create run.py

---
+++ run.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+#import navi, app
+
+from classes import bcolors
+
+print "----------------------------------------------------------------"
+print bcolors.OKGREEN +  "AzSaeed - Network Scanner - v1.0" + bcolors.ENDC
+print "----------------------------------------------------------------"
+import navi
